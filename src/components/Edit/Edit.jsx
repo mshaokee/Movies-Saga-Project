084cb95b2c84c987eb877caf0ddc3348b60ed5a0
Shaokee Moua <m.shaokee@gmail.com>
2020-05-15 23:55:09 -0500

    set defaultValue over value

--- src/components/Edit/Edit.jsx
+++ src/components/Edit/Edit.jsx
@@ -3,20 +3,47 @@ import { connect } from 'react-redux';
 
 class Edit extends Component {
 
+    state = {
+        title: this.props.reduxState.selectMovie.title,
+        description: this.props.reduxState.selectMovie.description
+    }
+
     componentDidMount() {
         console.log('Edit mounted', this.props.reduxState.selectMovie);
     };//end componentDidMount
 
+    changeTitle = (event) => {
+        console.log('in Edit changeTitle');
+        this.setState({
+            title: event.target.value
+        })
+    };//end changeTitle
+
+    changeDesc = (event) => {
+        console.log('in Edit changeDesc');
+        this.setState({
+            description: event.target.value
+        })
+    };//end changeDesc
+
     render() {
         return (
             <>
+                {/* TEST INPUT CHANGES */}
+                {JSON.stringify(this.state.title)};
+                {JSON.stringify(this.state.description)};
                 <div>
-                    <input type="text" placeholder={this.props.reduxState.selectMovie.title}/>
+                    <input onChange={this.changeTitle}
+                        type="text"
+                        defaultValue={this.props.reduxState.selectMovie.title}
+                    />
                 </div>
                 <div>
-                    <input type="text" placeholder={this.props.reduxState.selectMovie.description} />
+                    <input onChange={this.changeDesc}
+                        type="text"
+                        defaultValue={this.props.reduxState.selectMovie.description}
+                    />
                 </div>
-
             </>
         );//end return
     };//end render
